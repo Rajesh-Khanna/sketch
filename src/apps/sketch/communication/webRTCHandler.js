1285107f9c5236a0d002ba7fbbc0cd8ef843ab48
Rajesh Khanna <chrajeshkhanna82798@gmail.com>
2021-05-08 17:40:18 +0530

    stun servers added (#36)
    Co-authored-by: Rajesh-Khanna-ct <rajesh.chilaparasetty@cleartax.in>

--- src/apps/sketch/communication/webRTCHandler.js
+++ src/apps/sketch/communication/webRTCHandler.js
@@ -7,12 +7,18 @@ export default class RTC {
     localDescription;
 
     constructor(userType, onIce, onChannel, guestId) {
-        const configuration = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] }
+        const configuration = {
+            'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' },
+                { 'url': 'stun:stun1.l.google.com:19302' },
+                { 'url': 'stun:stun2.l.google.com:19302' },
+                { 'url': 'stun:stun3.l.google.com:19302' },
+                { 'url': 'stun:stun4.l.google.com:19302' },
+            ]
+        }
         this.rtc = new RTCPeerConnection(configuration);
 
-        this.rtc.onicecandidate = e => this.localDescription = JSON.stringify(this.rtc.localDescription);
-
         this.rtc.addEventListener("icegatheringstatechange", ev => {
+            console.log(this.rtc.iceGatheringState, ev);
             switch (this.rtc.iceGatheringState) {
                 case "complete":
                     onIce(this.rtc.localDescription, guestId);
@@ -26,11 +32,13 @@ export default class RTC {
             const dataChannel = this.createChannel('channel-name');
             console.log('on open set');
             this.rtc.addEventListener('connectionstatechange', event => {
+                console.log(this.rtc.connectionState, event);
                 if (this.rtc.connectionState === 'connected') {
                     console.log('connection established');
                     onChannel();
                 } else {}
             });
+
             dataChannel.onopen = (e) => { console.log('onOpen call'); };
         } else {
             this.rtc.ondatachannel = e => {
@@ -56,13 +64,16 @@ export default class RTC {
 
     setAnswer(answer) {
         console.log('setAnswer');
+        console.log(JSON.stringify(answer));
         this.rtc.setRemoteDescription(answer);
     }
 
     async setOffer(offer) {
         console.log('setOffer', offer);
+        console.log(JSON.stringify(offer));
         await this.rtc.setRemoteDescription(offer);
         const answer = await this.rtc.createAnswer();
+        console.log(JSON.stringify(answer));
         this.rtc.setLocalDescription(answer);
     }
 }
